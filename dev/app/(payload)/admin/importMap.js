c87f9cfbe129c6dad090beb53e78a580593b6fbe
David Stockley <magicspon@gmail.com>
2026-05-19 18:08:44 +0100

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'payload-navigation/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'payload-navigation/rsc'
+
+export const importMap = {
+  'payload-navigation/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'payload-navigation/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
